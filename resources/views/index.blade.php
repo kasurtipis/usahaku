8e6eb266a88bea1b24900d21ff42fe7748cff80d
KhoirulAfan <afan33222@gmail.com>
2025-10-21 20:41:57 +0700

    install react,tailwindcss

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Frontend React di Laravel</title>
+    @viteReactRefresh
+    @vite('resources/frontend/main.jsx')
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
